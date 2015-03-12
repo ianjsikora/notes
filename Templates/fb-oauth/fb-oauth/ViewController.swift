f475ae7cd0b917b6f84408e4c81a8df90e107fb8
ianjsikora <ianjsikora@gmail.com>
2015-03-11 18:06:51 -0700

    Mar 11, 2015: Facebook OAuth-Swift Template

---
+++ Templates/fb-oauth/fb-oauth/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  fb-oauth
+//
+//  Created by Ian Sikora on 3/11/15.
+//  Copyright (c) 2015 ianjsikora. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+class ViewController: UIViewController, FBLoginViewDelegate {
+    
+    @IBOutlet var fbLoginView: FBLoginView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+        
+    }
+    
+    // Facebook Delegate Methods
+    
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User: \(user)")
+        println("User ID: \(user.objectID)")
+        println("User Name: \(user.name)")
+        var userEmail = user.objectForKey("email") as String
+        println("User Email: \(userEmail)")
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
